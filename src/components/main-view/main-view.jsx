0640d5cba7f9b86e1d58417881c6ed792f2ad5ba
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2022-03-19 11:05:55 -0400

    3.3 commit
    Added several modules. In the process of troubleshooting app.

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { MovieCard } from 'src/components/movie-card/movie-card.jsx';
+
+export class MainView extends React.Component { 
+
+
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
+                { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
+                { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
+            ],
+            selectedMovie: null
+        }
+    }
+
+    render() {
+      const { movies } = this.state;
+    
+      if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    
+      return (
+        <div className="main-view">
+          {movies.map(movie => <MovieCard />)}
+        </div>
+      );
+    }
+    }
+    
+export default MainView;
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import  MainView  from 'src/components/main-view/main-view.jsx';
 
 // Import statement to indicate you need to bund.e './index.scss';
 import './index.scss';
+
 // Main component (will eventually use all the others)
 
 class MyFlixApplication extends React.Component {
     render() {
         return (
-            <div className="my-flix">
-            </div>
+          <MainView />
         );
     }
 }
